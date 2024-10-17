ebf5191435b506222a05f53251a6014bd29a7409
ahmadabbod7 <ahmadabbod7@gmail.com>
2024-10-17 18:56:50 +0300

    fixing path in userSclice and adding Provider in App

--- src/redux/slices/userSlice.js
+++ src/redux/slices/userSlice.js
@@ -1,5 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axiosInstance from "../../axiosInstance"; // Import your configured axios instance
+import axiosInstance from "../../services/axiosInstance"; // Import your configured axios instance
 
 // Async action for login
 export const login = createAsyncThunk(
