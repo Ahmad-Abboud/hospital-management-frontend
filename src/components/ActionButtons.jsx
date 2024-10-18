7ec7494a09b7e6b5c43140857ebd1453d93260e4
ahmadabbod7 <ahmadabbod7@gmail.com>
2024-10-18 04:44:15 +0300

    modifying component

--- src/components/ActionButtons.jsx
+++ src/components/ActionButtons.jsx
@@ -1,1 +1,22 @@
-ActionButtons;
+import React from "react";
+import { Button, Box } from "@mui/material";
+
+const ActionButtons = ({ onEdit, onDelete }) => {
+  return (
+    <Box>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={onEdit}
+        sx={{ mr: 1 }}
+      >
+        Edit
+      </Button>
+      <Button variant="contained" color="error" onClick={onDelete}>
+        Delete
+      </Button>
+    </Box>
+  );
+};
+
+export default ActionButtons;
